7e8484b4130f7f76ee18e6c2af0ea8d379422b6d
Al-Aiman (Ailiy) <alaimanramli6@gmail.com>
2025-10-10 00:17:03 +0800

    feat: add api function at frontend signup

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,6 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel.js";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { error } from "console";
 
 connect();
 
