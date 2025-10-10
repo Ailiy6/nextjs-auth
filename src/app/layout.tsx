0a4557d69ecae1f59b8e916347b7ff2fd70f3eee
Al-Aiman (Ailiy) <alaimanramli6@gmail.com>
2025-10-10 16:05:14 +0800

    feat: add logout api for backend put the api in profile page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <Toaster
           position="top-center"
           toastOptions={{
-            duration: 4000,
+            duration: 3000,
             style: {
               background: "#333",
               color: "#fff",
